d6ec944077d7fc1a67469283af15cc4eb3bb8663
Megan Dotto <megan.dotto@gmail.com>
2018-03-29 01:44:48 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -19,7 +19,7 @@ def second_supply_for_fourth_of_july(holiday_hash)
      }
    }
   # return the second element in the 4th of July array
-  [:holiday_supplies][:summer.values[2]]
+  [:holiday_supplies][:summer]
 end
 
 def add_supply_to_winter_holidays(holiday_hash, supply)
