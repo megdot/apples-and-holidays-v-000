a25aa7a6ee600445875b08ce5e490e0437fe290a
Megan Dotto <megan.dotto@gmail.com>
2018-03-29 02:50:48 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -68,9 +68,8 @@ def all_supplies_in_holidays(holiday_hash)
   # etc.
 
   holiday_hash[:winter].collect do |holiday, supplies|
-      supplies
-    end.flatten
-
+      supplies.flatten
+    end
 end
 
 def all_holidays_with_bbq(holiday_hash)
