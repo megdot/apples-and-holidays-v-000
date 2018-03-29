48b1866248c03aadf837d9da1f07788e10b81c89
Megan Dotto <megan.dotto@gmail.com>
2018-03-29 01:46:27 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -18,9 +18,9 @@ def second_supply_for_fourth_of_july(holiday_hash)
        :memorial_day => ["BBQ"]
      }
    }
- end
+
   # return the second element in the 4th of July array
-  [:holiday_supplies][:summer]
+
 end
 
 def add_supply_to_winter_holidays(holiday_hash, supply)
